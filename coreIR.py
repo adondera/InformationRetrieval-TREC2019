ca854ae37a7edd6264fbe1b085310e2884a45911
Alin Dondera <a.dondera@student.tudelft.nl>
2022-03-02 18:50:56 +0100

    add multi-threaded search to improve performance

--- coreIR.py
+++ coreIR.py
@@ -1,20 +1,28 @@
 from pyserini.search import SimpleSearcher
 import pandas as pd
+import time
+
+queries = pd.read_csv('queries.docdev.tsv', sep='\t', names=['query_id', 'query'], dtype={"query_id": str})
 
 searcher = SimpleSearcher('indexes/lucene-index-msmarco-doc')
 searcher.set_bm25(k1=4.46, b=0.82)
 
-queries = pd.read_csv('queries.docdev.tsv', sep='\t').to_numpy()
-
 counter = 0
-with open('output.trec', 'w') as fout:
-    for q in queries:
-        counter += 1
-        if counter % 10 == 0:
-            print(counter)
-        hits = searcher.search(q[1], k=100)
-        for (idx, hit) in enumerate(hits):
-            score = 1.0 / int(idx + 1)
-            fout.write(f'{q[0]} Q0 {hit.docid} {idx} {score} anserini\n')
 
+start = time.time()
 
+batch_size = 64
+threads = 12
+with open('output4.trec', 'w') as fout:
+    i = 0
+    while i < len(queries):
+        batch = queries.iloc[i:min(i + batch_size, len(queries))]
+        hits = searcher.batch_search(queries=batch['query'].to_list(), qids=batch['query_id'].to_list(), threads=8,
+                                     k=100)
+        for q_id, results in hits.items():
+            for (idx, hit) in enumerate(results):
+                score = 1.0 / int(idx + 1)
+                fout.write(f'{q_id} Q0 {hit.docid} {idx} {score} anserini\n')
+        i += batch_size
+        print(f'{i} queries processed')
+print(f'Took {time.time() - start}s to finish')
